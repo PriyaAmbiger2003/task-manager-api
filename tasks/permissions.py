4e41a74df44224ecf3b31ab4e99a98068cc20ff3
Priya Ambiger <priyaambiger86@gmail.com>
2026-03-25 09:58:14 +0530

    permissions.py

--- tasks/permissions.py
+++ tasks/permissions.py
@@ -4,15 +4,15 @@ class IsTaskPermission(BasePermission):
 
     def has_object_permission(self, request, view, obj):
 
-        # ✅ Read-only requests → allow (GET, HEAD, OPTIONS)
+        
         if request.method in SAFE_METHODS:
             return True
 
-        # ✅ DELETE → only creator
+        
         if request.method == "DELETE":
             return obj.created_by == request.user
 
-        # ✅ UPDATE (PUT/PATCH)
+       
         if request.method in ["PUT", "PATCH"]:
             # Assigned user OR creator can attempt update
             return (
@@ -20,5 +20,5 @@ class IsTaskPermission(BasePermission):
                 obj.created_by == request.user
             )
 
-        # ❌ Everything else denied
+        
         return False
